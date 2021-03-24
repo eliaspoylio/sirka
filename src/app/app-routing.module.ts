66c921c470aae20c725283e7a301435bb3549236
shintsal <70944299+shintsal@users.noreply.github.com>
2021-03-24 10:49:02 +0200

    add components

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { FormArray } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+import { FavouritesComponent } from './favourites/favourites.component';
+import { FrontpageComponent } from './frontpage/frontpage.component';
+import { ItemsComponent } from './items/items.component';
+import { LoginComponent } from './login/login.component';
+import { MapComponent } from './map/map.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
+import { UserInfoComponent } from './user-info/user-info.component';
+
+
+const routes: Routes = [
+  {path: '', redirectTo: '/frontpage', pathMatch: 'full'},
+  {path: '/frontpage', component: FrontpageComponent},
+  {path: '/items', component: ItemsComponent },
+  {path: '/favourites', component: FavouritesComponent},
+  {path: '/login', component: LoginComponent},
+  {path: '/map', component: MapComponent},
+  {path: '/navbar', component: NavbarComponent},
+  {path: '/searchbar', component: SearchbarComponent},
+  {path: '/user-info', component: UserInfoComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
+export const routingComponents = [ FrontpageComponent, ItemsComponent, FavouritesComponent, LoginComponent, MapComponent, SearchbarComponent, UserInfoComponent ]
---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
+import { FavouritesComponent } from './favourites/favourites.component';
+import { ItemsComponent } from './items/items.component';
+import { UserInfoComponent } from './user-info/user-info.component';
+import { FrontpageComponent } from './frontpage/frontpage.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MapComponent,
+    LoginComponent,
+    NavbarComponent,
+    SearchbarComponent,
+    FavouritesComponent,
+    ItemsComponent,
+    UserInfoComponent,
+    FrontpageComponent,
+    Page1Component,
+    Page2Component,
+    Page3Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
