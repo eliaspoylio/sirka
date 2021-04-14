1f1606d96f269de36de72917d405d86bd38b2457
Aleksandra Dahlqvist <s2000575@edu.bc.fi>
2021-04-14 13:25:06 +0300

    set activities to fetch data from open-apiHelsinki

--- src/app/_services/activity.service.ts
+++ src/app/_services/activity.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ActivityService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+   apiURL = "http://open-api.myhelsinki.fi/v1";
+
+   public getActivities() {
+     return this.httpClient.get('https://sirka-proxy.herokuapp.com/' + this.apiURL + "/activities/")
+   };
 }
+
--- src/app/activities/activities.component.ts
+++ src/app/activities/activities.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { IApiData } from '../_models/apidata';
+import { IActivity } from '../_models/activity';
+import { ActivityService } from '../_services/activity.service';
+import { element } from 'protractor';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-activities',
@@ -6,10 +11,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./activities.component.css']
 })
 export class ActivitiesComponent implements OnInit {
+  activitySubscription: Subscription
+  activities: IActivity[] = [];
 
-  constructor() { }
+  constructor(private activityService: ActivityService) { }
 
   ngOnInit(): void {
+    this.activitySubscription = this.activityService.getActivities().subscribe((data) => {
+      let activityData = data as IApiData;
+
+      activityData.data.forEach(element => {
+        this.activities.push(element);
+      });
+      this.activities.forEach(element => {
+
+      });
+    }, error => {
+      console.log(error);
+    });
   }
 
-}
+};
