4b5c5651b45f113a65807f5b584916b81f5b97b6
Aleksandra Dahlqvist <s2000575@edu.bc.fi>
2021-04-14 12:30:53 +0300

    set up event to fetch data from open-api-helsinki

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { IEvent } from '../_models/event';
+import { IApiData } from '../_models/apidata';
+import { element } from 'protractor';
+import { Subscription } from 'rxjs';
+import { EventService } from '../_services/event.service';
 
 @Component({
   selector: 'app-events',
@@ -6,10 +11,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./events.component.css']
 })
 export class EventsComponent implements OnInit {
+  eventSubscription: Subscription;
+  events: IEvent[] = [];
 
-  constructor() { }
+  constructor(private eventService: EventService) { }
 
   ngOnInit(): void {
+    this.eventSubscription = this.eventService.getEvents().subscribe((data) => {
+      let eventData = data as IApiData;
+
+      eventData.data.forEach(element => {
+        this.events.push(element);
+      });
+      this.events.forEach(element => {
+
+      });
+    }, error => {
+      console.log(error);
+    });
   }
 
-}
+};
