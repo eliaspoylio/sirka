98b07cf3a3646c3390a45149d56bd3283f0a9170
Auli Färm <farmauli@gmail.com>
2021-04-22 10:41:47 +0300

    Add weather service references to weather.component.ts -file

--- src/app/_services/weather.service.ts
+++ src/app/_services/weather.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class WeatherService {
 
-  APIkey = '<insert_api_key>';
+  APIkey = '<INSERT_APIKEY_HERE>';
   URL = 'https://api.openweathermap.org/data/2.5/onecall?lat=60.1695&lon=24.9355&exclude=minutely,daily,alerts&appid=';
 
   constructor(private http: HttpClient) { }
