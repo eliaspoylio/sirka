44d68e675e663077b432723a7ae093435a5b0ef4
Aleksandra Dahlqvist <s2000575@edu.bc.fi>
2021-05-10 12:42:56 +0300

    add searchbar to activities component

--- src/app/categories/activities/activities.component.ts
+++ src/app/categories/activities/activities.component.ts
@@ -14,6 +14,7 @@ import { StripPipe } from '../../_pipes/strip.pipe';
 export class ActivitiesComponent implements OnInit {
   activitySubscription: Subscription
   activities: IActivity[] = [];
+  searchText;
 
   constructor(private activityService: ActivityService) { }
 
