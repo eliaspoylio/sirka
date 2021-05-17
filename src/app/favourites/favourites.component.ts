33d6bb954664aacb84fd78a47ffc89526f5cf6a6
Elias Pöyliö <elias.poylio@gmail.com>
2021-05-17 23:07:19 +0300

    display favourites

--- src/app/favourites/favourites.component.ts
+++ src/app/favourites/favourites.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PlaceService } from '../_services/place.service'
+import { EventService } from '../_services/event.service'
+import { ActivityService } from '../_services/activity.service'
 import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { FavouriteService } from '../_services/favourite.service';
@@ -9,6 +11,7 @@ import { Subscription } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Favourite } from '../_models/favourite';
 
+
 @Component({
   selector: 'app-favourites',
   templateUrl: './favourites.component.html',
@@ -16,10 +19,15 @@ import { Favourite } from '../_models/favourite';
 })
 export class FavouritesComponent implements OnInit {
   favourites = [];
+  favouriteSubscription: Subscription;
+
 
   constructor(
     private favouriteService: FavouriteService,
-    private httpClient: HttpClient
+    private httpClient: HttpClient,
+    private placeService: PlaceService,
+    private eventService: EventService,
+    private activityService: ActivityService,
   ) { }
 
   ngOnInit(): void {
@@ -27,25 +35,33 @@ export class FavouritesComponent implements OnInit {
     console.log("Angular 10 Promises");
     this.fetchDataAsPromise()
       .then((data) => {
-        
-        console.log(JSON.stringify(data));
-        console.log(Object.values(data))
-        Object.values(data).forEach(element => {
-          this.favourites.push(element);
-        });
-        console.log(this.favourites)
-        
+
         Object.values(data).forEach(element => {
           if (element.category == "places") {
-            console.log(element.category)
+            this.favouriteSubscription = this.placeService.getOnePlace(element.favId).subscribe((data) => {
+              this.favourites.push(data)
+              console.log("pushed", data)
+            }, error => {
+              console.log(error);
+            });
           }
           else if (element.category == "events") {
-            console.log(element.category)
+            this.favouriteSubscription = this.eventService.getOneEvent(element.favId).subscribe((data) => {
+              this.favourites.push(data)
+              console.log("pushed", data)
+            }, error => {
+              console.log(error);
+            });
           }
           else if (element.category == "activities") {
-            console.log(element.category)
+            this.favouriteSubscription = this.activityService.getOneActivity(element.favId).subscribe((data) => {
+              this.favourites.push(data)
+              console.log("pushed", data)
+            }, error => {
+              console.log(error);
+            });
           }
-          
+
 
         });
       })
