c33bd75e5462d452ce5d630315aa143ff21767f2
Elias Pöyliö <elias.poylio@gmail.com>
2021-05-19 23:45:59 +0300

    http -> https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   //apiUrl: 'http://sirka-proxy.herokuapp.com/api'
-  apiUrl: 'http://sirkabackend.herokuapp.com/api'
+  apiUrl: 'https://sirkabackend.herokuapp.com/api'
 };
