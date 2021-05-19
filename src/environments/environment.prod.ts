bea326115afcf422c947c7a6110713eed3473075
Elias Pöyliö <elias.poylio@gmail.com>
2021-05-19 23:28:46 +0300

    quick fix for angular environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://sirka-proxy.herokuapp.com/api'
+  //apiUrl: 'http://sirka-proxy.herokuapp.com/api'
+  apiUrl: 'http://sirkabackend.herokuapp.com/api'
 };
